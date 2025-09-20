d002b2bbe6d206a9831371cce753488a325c3cce
DominicDev <s0l0estudi4r@gmail.com>
2025-09-20 23:17:40 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = ' FUEGO';
+export let nameSongs = [
+	"UNA VAINA LOCA", /*1*/
+	"GOOD VIBES", /*2*/
+	"MAMBO PARA BAILAR", /*3*/
+	"35 PA LAS 12", /*4*/
+	"U LA LA", /*5*/
+	"PRÉNDELO", /*6*/
+	"MAMBO PARA BAILAR", /*7*/
+	"CIEN", /*8*/
+	"SIGO FRESH", /*9*/
+	"CEREZA", /*10*/
+	"LOCO POR LLEGAR", /*11*/
+	"BIEN FANCY", /*12*/
+	"NUEVO", /*13*/
+	"LA WEED", /*14*/
+	"SIN PARAR", /*15*/
+	"ICE CREAM", /*16*/
+	"TEDDI", /*17*/
+	"WEEKEND", /*18*/
+	"FLEX", /*19*/
+	"GOLD RUSH", /*20*/
+]
